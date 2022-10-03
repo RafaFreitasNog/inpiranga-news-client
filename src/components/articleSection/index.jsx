26d65153dbafbe3b97561a4f97ed1ce2e100eafb
Raphael Freitas Nogueira de Souza <raphaelfreitas18.dev@gmail.com>
2022-10-02 23:32:03 -0300

    Changed some issues with the Header grid, and started working on the edit and delete articles features

--- src/components/articleSection/index.jsx
+++ src/components/articleSection/index.jsx
@@ -10,9 +10,13 @@ function ArticleSection(props) {
         <div id="article-section-title-conteiner">
           <h2 className='bold'>{props.article.title}</h2>
           <h6 id='article-section-subtitle'>{props.article.subtitle}</h6>
-          <p id='article-section-author-info' className='bold'>By {props.article.author[0].name}, Ipiranga News</p>
-          <p id='article-section-time-info' className='small italic'>Published {moment(props.article.created_at).format('ll')}, updated {moment(props.article.created_at).fromNow()}</p>
-          { props.isOwner && <p>Yours</p>}
+          <div id='article-section-info-icons'>
+            <div id='article-section-info-conteiner'>
+              <p id='article-section-author-info' className='bold'>By {props.article.author[0].name}, Ipiranga News</p>
+              <p id='article-section-time-info' className='small italic'>Published {moment(props.article.created_at).format('ll')}, updated {moment(props.article.created_at).fromNow()}</p>
+            </div>
+            { props.isOwner && <p>Yours</p>}
+          </div>
         </div>
         <div></div>
         <div id="article-section-body-conteiner">
